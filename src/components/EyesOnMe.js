01fb6b0f0cc6dd9e04aa633c9a86df5b1391d856
iam <brian.kibet2@student.moringaschool.com>
2023-07-20 04:21:57 +0000

    completed task

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  const handleFocus = () => {
+    console.log('Good!');
+  };
+
+  const handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <div>
+      <h2>EyesOnMe Component</h2>
+      <button onFocus={handleFocus} onBlur={handleBlur}>
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
-
+import React from "react";
 function Keypad (){
+    const handlePasswordChange = () =>{
+        console.log('Entering password...');
+    };
     return (
-        <div></div>
-    )
+        <div>
+            <h2>Keypad Component</h2>
+            <input type="password" on onChange={handlePasswordChange}/>
+
+        </div>
+    
+    );
 }
 
 export default Keypad;
